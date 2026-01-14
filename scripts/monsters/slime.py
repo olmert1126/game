f5950970cfa39eae718347faf96d7490c47c1dd3
olmer <nicitoxa@yandex.ru>
2026-01-14 20:14:08 +0500

    кароч анимацию атаки сделал, но мой внутренний дизайнер повесился

--- scripts/monsters/slime.py
+++ scripts/monsters/slime.py
@@ -27,6 +27,16 @@ class Slime:
         self.texture_change_delay_run = 0.2
         self.run = True
 
+        self.anim_attack_right = [self.one_shot_attack, self.two_shot_attack]
+        self.anim_attack_left = [self.one_shot_attack_left, self.two_shot_attack_left]
+
+        self.is_attacking = False
+        self.attack_duration = 0.4  # общая длительность анимации атаки
+        self.attack_timer = 0.0
+        self.current_texture_attack = 0
+        self.texture_change_time_attack = 0
+        self.texture_change_delay_attack = self.attack_duration / len(self.anim_attack_right)
+
         self.slime_sprite = arcade.Sprite()
         self.slime_sprite.texture = self.main_form
         self.slime_sprite.center_x = self.center_x
@@ -70,6 +80,11 @@ class Slime:
         self.three_shot_run_left = self.three_shot_run.flip_left_right()
         self.four_shot_run_left = self.four_shot_run.flip_left_right()
 
+        self.one_shot_attack = arcade.load_texture("models/monsters/slime/animations/take_damage/anim_attack1.png")
+        self.two_shot_attack = arcade.load_texture("models/monsters/slime/animations/take_damage/anim_attack2.png")
+        self.one_shot_attack_left = self.one_shot_attack.flip_left_right()
+        self.two_shot_attack_left = self.two_shot_attack.flip_left_right()
+
     def on_update(self, delta_time):
         # Обновляем физику
         self.physics_engine.update()
@@ -94,6 +109,11 @@ class Slime:
                 continue
 
             if arcade.check_for_collision(self.slime_sprite, player.player_sprite):
+                if not self.is_attacking:
+                    self.is_attacking = True
+                    self.attack_timer = 0.0
+                    self.current_texture_attack = 0
+
                 self._last_damage_times[i] += delta_time
                 if self._last_damage_times[i] >= self.damage_cooldown:
                     player.take_damage(self.damage)
@@ -103,16 +123,36 @@ class Slime:
         self.facing = "left" if self.facing == "right" else "right"
         self.current_texture_run = 0
 
-    def update_animation(self, delta_time: float = 1 / 60):
-        self.texture_change_time_run += delta_time
-        if self.texture_change_time_run >= self.texture_change_delay_run:
-            self.texture_change_time_run = 0
-            self.current_texture_run = (self.current_texture_run + 1) % len(self.anim_run_right)
+    def update_animation(self, delta_time: float):
+        if self.is_attacking:
+            self.attack_timer += delta_time
+            self.texture_change_time_attack += delta_time
 
-            if self.facing == "right":
-                self.slime_sprite.texture = self.anim_run_right[self.current_texture_run]
-            else:
-                self.slime_sprite.texture = self.anim_run_left[self.current_texture_run]
+            if self.texture_change_time_attack >= self.texture_change_delay_attack:
+                self.texture_change_time_attack = 0
+                self.current_texture_attack = (self.current_texture_attack + 1) % len(self.anim_attack_right)
+
+                if self.facing == "right":
+                    self.slime_sprite.texture = self.anim_attack_right[self.current_texture_attack]
+                else:
+                    self.slime_sprite.texture = self.anim_attack_left[self.current_texture_attack]
+
+            # Завершаем анимацию по времени
+            if self.attack_timer >= self.attack_duration:
+                self.is_attacking = False
+                self.current_texture_run = 0  # сброс индекса бега
+
+        else:
+            # Обычная анимация бега
+            self.texture_change_time_run += delta_time
+            if self.texture_change_time_run >= self.texture_change_delay_run:
+                self.texture_change_time_run = 0
+                self.current_texture_run = (self.current_texture_run + 1) % len(self.anim_run_right)
+
+                if self.facing == "right":
+                    self.slime_sprite.texture = self.anim_run_right[self.current_texture_run]
+                else:
+                    self.slime_sprite.texture = self.anim_run_left[self.current_texture_run]
 
     def draw(self):
         self.slime_sprites_list.draw()
